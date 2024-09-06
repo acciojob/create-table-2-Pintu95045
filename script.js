406ddc8a49fbce6ce099e3a3d51bdc21ae5caf06
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-09-06 21:49:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function createTable() {
+function insert_Row() {
     //Write your code here
+	 // select the table
+    var table = document.getElementById("sampleTable");
+
+    // create a new row
+    var row = table.insertRow(0);
+
+    // create two cells
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+
+    // set the text of the cells
+    cell1.textContent = "New Cell1";
+    cell2.textContent = "New Cell2";
   
 }
